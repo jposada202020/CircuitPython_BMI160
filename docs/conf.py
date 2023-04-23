aa5cc07072c75ae45c75d0379313da2c32201426
jposada202020 <34255413+jposada202020@users.noreply.github.com>
2023-04-22 22:00:57 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -105,7 +105,7 @@ html_theme_options = {
         {
             "media": "(prefers-color-scheme: light)",
             "scheme": "default",
-            "primary": "green",
+            "primary": "purple",
             "accent": "light-blue",
             "toggle": {
                 "icon": "material/lightbulb-outline",
